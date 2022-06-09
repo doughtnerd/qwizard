8bc470f0a705c8055a8a8b92f9070908d82fb337
Chris Carlson <ccarlson@Chris-Cs-MBP.lan>
2022-06-08 22:59:24 -0700

    Svelte projects working

---
+++ packages/svelte-example/jest.config.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+export default {
+  displayName: 'svelte-example',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(.+\\.svelte$)': [
+      'svelte-jester',
+      {
+        preprocess: 'packages/svelte-example/svelte.config.cjs',
+      },
+    ],
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['svelte', 'ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/svelte-example',
+};
